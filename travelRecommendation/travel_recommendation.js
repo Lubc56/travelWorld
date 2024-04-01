07aeeea436c0ba2a51ae63b8f297285d3fd7f01e
Luigi <luigi.bulla@gmail.com>
2024-03-31 22:11:19 -0500

    add functions

--- travelRecommendation/travel_recommendation.js
+++ travelRecommendation/travel_recommendation.js
@@ -1,15 +1,17 @@
 const xhrBtn = document.getElementById("btnSearch")
+const clearBtn = document.getElementById("btnClear")
 const userInput = document.getElementById("cityInput")
+
 
 
 function getText() {
     var taskText = userInput.value.trim();
-    if(taskText.toLowerCase() == "beaches"){
-        taskText="beach";
-    } else if (taskText.toLowerCase() == "cities" || taskText.toLowerCase() == "country" ){
-        taskText="city";
-    } else if (taskText.toLowerCase() == "temples"){
-        taskText="temple";
+    if (taskText.toLowerCase() == "beaches") {
+        taskText = "beach";
+    } else if (taskText.toLowerCase() == "cities" || taskText.toLowerCase() == "country") {
+        taskText = "city";
+    } else if (taskText.toLowerCase() == "temples") {
+        taskText = "temple";
     }
     return taskText.toLowerCase()
 }
@@ -25,18 +27,54 @@ xhrBtn.addEventListener("click", function () {
     xhr.responseType = 'json';
 
     xhr.onload = function () {
-
+        var resultsDiv = document.getElementById("containerResults")
 
         switch (getText()) {
             case "city":
-                var cities = xhr.response.countries;
-                cities.forEach(function (city) {
+                var country = xhr.response.countries;
+                country.forEach(function (city) {
+                    city.cities.forEach(function (citiesA) {
+                        var infoResult = document.createElement('div');
+                        infoResult.classList.add('infoResult');
+                        var resultimg = document.createElement('img');
+                        var resulth2 = document.createElement('h2');
+                        var resultp = document.createElement('p');
+                        const resultbutton = document.createElement('button');
+                        resultbutton.textContent = "visit"
+
+                        resultimg.src = citiesA.imageUrl
+                        resulth2.textContent = citiesA.name
+                        resultp.textContent = citiesA.description
+
+                        infoResult.appendChild(resultimg)
+                        infoResult.appendChild(resulth2)
+                        infoResult.appendChild(resultp)
+                        infoResult.appendChild(resultbutton)
+                        resultsDiv.appendChild(infoResult)
+                    })
                     console.log(city.name)
                 })
                 break;
             case "temple":
                 var temples = xhr.response.temples;
                 temples.forEach(function (temple) {
+                    var infoResult = document.createElement('div');
+                    infoResult.classList.add('infoResult');
+                    var resultimg = document.createElement('img');
+                    var resulth2 = document.createElement('h2');
+                    var resultp = document.createElement('p');
+                    const resultbutton = document.createElement('button');
+                    resultbutton.textContent = "visit"
+
+                    resultimg.src = temple.imageUrl
+                    resulth2.textContent = temple.name
+                    resultp.textContent = temple.description
+
+                    infoResult.appendChild(resultimg)
+                    infoResult.appendChild(resulth2)
+                    infoResult.appendChild(resultp)
+                    infoResult.appendChild(resultbutton)
+                    resultsDiv.appendChild(infoResult)
                     console.log(temple.name)
                 })
                 break;
@@ -44,6 +82,23 @@ xhrBtn.addEventListener("click", function () {
             case "beach":
                 var beaches = xhr.response.beaches;
                 beaches.forEach(function (beach) {
+                    var infoResult = document.createElement('div');
+                    infoResult.classList.add('infoResult');
+                    var resultimg = document.createElement('img');
+                    var resulth2 = document.createElement('h2');
+                    var resultp = document.createElement('p');
+                    const resultbutton = document.createElement('button');
+                    resultbutton.textContent = "visit"
+
+                    resultimg.src = beach.imageUrl
+                    resulth2.textContent = beach.name
+                    resultp.textContent = beach.description
+
+                    infoResult.appendChild(resultimg)
+                    infoResult.appendChild(resulth2)
+                    infoResult.appendChild(resultp)
+                    infoResult.appendChild(resultbutton)
+                    resultsDiv.appendChild(infoResult)
                     console.log(beach.name)
                 })
                 break;
@@ -57,5 +112,10 @@ xhrBtn.addEventListener("click", function () {
     xhr.send()
 });
 
-
+function clearResults() {
+    var resultsDiv = document.getElementById("containerResults")
+    while (resultsDiv.firstChild) {
+        resultsDiv.removeChild(resultsDiv.firstChild);
+    }
+}
 
